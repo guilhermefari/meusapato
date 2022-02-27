9246864c51eabe683c7371cfb75506586dec6a69
Guilherme Fonseca <guilhermefari@outlook.com.br>
2022-02-27 17:18:00 -0300

    first screen for orders

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const navigate = (location) => document.location=`${location}`
+
+const displayAddress = () => {
+    const checkBox = document.getElementById("displayAddress")
+    if(checkBox.checked)
+        document.getElementById('endereco-alternativo').style.display = "none"
+    else
+    document.getElementById('endereco-alternativo').style.display = "block"
+}
