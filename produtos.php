4d05557e3a2ae1c4508b704ce90cbd33c1a51912
Guilherme Fonseca <guilhermefari@outlook.com.br>
2022-04-03 18:25:38 -0300

    fixing bug on product details

--- produtos.php
+++ produtos.php
@@ -69,7 +69,7 @@
                         echo "<p>Público: {$row[3]}</p>";
                         echo "<p>Tipo de fechamento: {$row[4]}</p>";
 
-                        if($row[5]){
+                        if($row[5] == 't'){
                             echo "<div style='display: flex'>";
                                 echo "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' class='bi bi-check' color='green' viewBox='0 0 16 16'>
                                 <path d='M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z'/>
@@ -77,7 +77,7 @@
                                 echo "<p>Amortecedor</p>";
                             echo "</div>";
                         }
-                        if($row[6]){
+                        if($row[6] == 't'){
                             echo "<div style='display: flex'>";
                                 echo "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' class='bi bi-check' color='green' viewBox='0 0 16 16'>
                                 <path d='M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z'/>
